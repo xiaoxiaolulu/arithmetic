ee533dbb452338fca76a7c7982c38428791b617d
Null <546464268@qq.com>
2020-02-09 19:59:25 +0800

    算法强化01

--- Check12.py
+++ Check12.py
@@ -1,3 +1,25 @@
 """
 
 """
+
+
+class EmployeeManagement:
+
+    raise_pct = 1.05
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def fullname(self):
+        return "{}{}".format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_pct)
+
+    def __repr__(self):
+        return "EmployeeManagement('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return "{}-{}".format(self.fullname(), self.pay)
---
+++ Check13.py
@@ -0,0 +1,7 @@
+
+
+def another_func_to_sum(n):
+
+    final_result = (1+n)*n*0.5
+    return final_result
+
