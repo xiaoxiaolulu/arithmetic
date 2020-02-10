5b96500f725184c905113e895b8aba922c76aa55
Null <546464268@qq.com>
2020-02-10 15:00:06 +0800

    基础强化

---
+++ Check14.py
@@ -0,0 +1,10 @@
+
+
+def find_the_match(search_list, match_elem):
+
+    for elem in search_list:
+        if elem == match_elem:
+            print(True)
+        print(False)
+
+find_the_match([1, 2, 3, 4, 5, 6, 7], 1)
